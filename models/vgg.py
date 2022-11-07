6ff0258ad679efdf92d08ee880c2ec628dc8bad9
NguyenTriQuan <nguyentriquan99@gmail.com>
2022-11-07 10:46:22 +0700

    mini

--- models/vgg.py
+++ models/vgg.py
@@ -6,7 +6,7 @@ import numpy as np
 
 __all__ = [
     'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
-    'vgg19_bn', 'vgg19', 'custom_vgg', 'custom_vgg_cifar100'
+    'vgg19_bn', 'vgg19', 'custom_vgg', 'custom_vgg_cifar100', 'custom_vgg_mini_imagenet'
 ]
 
 
